f33849a9e6d78d065b183fc444e0009354e7bc02
Shaikat Majumder <68814937+shaikat010@users.noreply.github.com>
2021-12-06 01:57:48 +0600

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,16 @@
+
+let day = "Monday"
+
+// code for deicidng what time to wake up on each day.
+switch(day){
+    case("Monday"):
+    console.log("Wake up early")
+    break // this command is given as without it the code will breakthrough 
+            //and print every case after the match.
+
+    case("Friday"):
+    console.log("Wake up anytime")
+}
+
+console.log("Execution complete")
+
