5ea820b5f5d9aab7dd7072fdfd749fcaf17d9ba7
Shaikat Majumder <68814937+shaikat010@users.noreply.github.com>
2021-12-05 13:55:59 +0600

    Create conditional-statements.js

---
+++ conditional-statements.js
@@ -0,0 +1,24 @@
+
+
+// finding the greatest number
+
+let num1 = 10
+let num2 = 30
+let num3 = 20
+
+if(num1 > num2 && num1 > num3){
+    console.log("num1 is the greatest")
+}
+
+
+else if(num2 > num3){
+    console.log("num2 is the greatest")
+}
+
+
+else{
+    console.log("num3 is the greatest")
+}
+
+
+console.log("BYE")
