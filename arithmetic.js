045646aecb6163c9b4e492fe1c8a23ffca5dbeb1
Shaikat Majumder <68814937+shaikat010@users.noreply.github.com>
2021-12-05 13:56:28 +0600

    Create arithmetic.js

---
+++ arithmetic.js
@@ -0,0 +1,21 @@
+
+
+let x = 2
+let y = 3
+
+console.log( x + y)
+console.log(x -y)
+console.log(x/y)
+console.log(x%y)
+
+let num = true
+let num2 = true
+console.log(num + num2) //true == 1 here
+
+// all of the nelow are power of 2
+let A  = 2
+console.log(2*2*2)
+console.log(Math.pow(2,3))
+console.log(2**3)
+
+
