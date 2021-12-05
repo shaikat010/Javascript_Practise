a2cd865022f61e5f6163634cfac5205d9dcceec7
Shaikat Majumder <68814937+shaikat010@users.noreply.github.com>
2021-12-05 13:58:04 +0600

    Create logical-operators.js

---
+++ logical-operators.js
@@ -0,0 +1,16 @@
+
+let x = 5, y =2, z = 3
+
+// this is the or operator
+result = x > y || y > z
+console.log(result)
+
+
+// this is the and operator
+result = x > y && y > z
+console.log(result)
+
+
+// this is the not operator
+final = !result
+console.log(final)
