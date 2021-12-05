982852e1a67afc7c34702319ee55cdeb4bec8025
Shaikat Majumder <68814937+shaikat010@users.noreply.github.com>
2021-12-05 13:55:29 +0600

    Create data-types.js

---
+++ data-types.js
@@ -0,0 +1,24 @@
+
+
+// setting values using hexadecimals
+
+let num = 0xf
+console.log(num) //prints 15
+
+
+console.log(Number.MAX_VALUE)
+
+
+let x = 13133424535768567345234n  // this is a BigInt representaiton of the number.
+console.log(x)
+
+
+let user = "Shaikat \"Majumder\"" //way of placing quotaiton inside the string content
+console.log(user)
+
+
+let name
+console.log(name, typeof name)
+
+name = 10
+console.log(name)
