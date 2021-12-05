5d4dbc8f51e5f2854926c27e5ee7ae726957773d
Shaikat Majumder <68814937+shaikat010@users.noreply.github.com>
2021-12-05 13:58:33 +0600

    Create relational-operators.js

---
+++ relational-operators.js
@@ -0,0 +1,10 @@
+let comp = 5
+let comp2 = 5
+console.log(comp > comp2)
+
+
+// we use triple equal to in javascript instead of double equal to,
+// never use double equal to.
+let x = '3'
+let y = 3
+console.log(x === y)
